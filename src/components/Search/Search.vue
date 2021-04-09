60b47fb010c3feb3de72c64abd0b2a3760f47ace
Kachanov2508 <Kachanov2508@yandex.ru>
2021-04-09 17:55:51 +0300

    ok

--- src/components/Search/Search.vue
+++ src/components/Search/Search.vue
@@ -16,24 +16,37 @@ export default {
 </script>
 
 <style>
-.search {
-  display: grid;
-  grid-template-columns: 790px 1fr;
-  align-items: center;
-  column-gap: 20px;
-	margin-top: 50px;
-}
+  .search {
+    display: grid;
+    grid-template-columns: 790px 1fr;
+    align-items: center;
+    column-gap: 20px;
+    margin-top: 50px;
+  }
 
-input {
-  width: 100%;
-  height: 75px;
-}
+  input {
+    width: 100%;
+    height: 75px;
+    background: none;
+    font-size: 30px;
+    font-weight: 300;
+    color: inherit;
+    border: none;
+    outline: none;
+    box-shadow: inset -5px -5px 15px 0 #ffffff, inset 5px 5px 15px 0 #c6c6c6;
+    border-radius: 8px;
+    padding: 20px 20px 20px 40px;
+    opacity: .6;
+  }
 
-.logo {
-  font-size: 50px;
-  color: #4C80A4;
-  font-weight: 700;
-  text-transform: uppercase;
-}
+  input::placeholder{
+    /* opacity: .6; */
+  }
 
+  .logo {
+    font-size: 50px;
+    color: #4C80A4;
+    font-weight: 700;
+    text-transform: uppercase;
+  }
 </style>
